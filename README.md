a8a5d6489b1b99aa0c1dd1d41a612fa378c9db3d
Mike Funk <mfunk@xulonpress.com>
2012-02-17 21:25:02 -0500

    added readme and sql dump.

---
+++ README.md
@@ -0,0 +1,15 @@
+Bookymark is a sample application created for a few reasons:
+
+* Show a sample of my code
+* Share an example of CodeIgniter best practices
+* Get your pull requests on what are better practices :)
+* Practice new techniques
+* Simplify as much as possible
+* Modularize code into libraries/packages/helpers whenever possible
+* Get used to writing tests before writing code
+* No deadlines
+* No pressure to compete with other services or be successful
+* No ever-growing roadmap of features, just the basics
+* Eventually port the complete application to other languages and frameworks as an exercise
+
+Run **setup.sql**, update **development/database.php** and navigate to the root and you should be good to go.
---
+++ setup.sql
@@ -0,0 +1,108 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 3408
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.5.19)
+# Database: mikedfunk_db
+# Generation Time: 2012-02-17 21:24:42 -0500
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table bookmarks
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `bookmarks`;
+
+CREATE TABLE `bookmarks` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(300) DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `bookmarks` WRITE;
+/*!40000 ALTER TABLE `bookmarks` DISABLE KEYS */;
+
+INSERT INTO `bookmarks` (`id`, `url`, `description`)
+VALUES
+	(1,'http://yahoo.com','it\'s yahoo!'),
+	(2,'http://yahoo.com','it\'s yahoo!'),
+	(3,'http://yahoo.com','it\'s yahoo!'),
+	(4,'http://yahoo.com','it\'s yahoo!'),
+	(5,'http://yahoo.com','it\'s yahoo!');
+
+/*!40000 ALTER TABLE `bookmarks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table roles
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `roles`;
+
+CREATE TABLE `roles` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) DEFAULT NULL,
+  `home_page` varchar(100) DEFAULT NULL,
+  `can_list_bookmarks` tinyint(1) DEFAULT '0',
+  `can_view_bookmarks` tinyint(1) DEFAULT '0',
+  `can_edit_bookmarks` tinyint(1) DEFAULT '0',
+  `can_add_bookmarks` tinyint(1) DEFAULT '0',
+  `can_delete_bookmarks` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+
+INSERT INTO `roles` (`id`, `title`, `home_page`, `can_list_bookmarks`, `can_view_bookmarks`, `can_edit_bookmarks`, `can_add_bookmarks`, `can_delete_bookmarks`)
+VALUES
+	(1,'Superuser','bookmarks/list_bookmarks?notification=login_success',1,1,1,1,1);
+
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `email_address` varchar(300) DEFAULT NULL,
+  `password` varchar(300) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+
+INSERT INTO `users` (`id`, `email_address`, `password`, `role_id`)
+VALUES
+	(1,'test','test',1);
+
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
