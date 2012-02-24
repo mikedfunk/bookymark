a0dd2e11d5ee72b5c8129b63bb241db703c372ac
Mike Funk <mfunk@xulonpress.com>
2012-02-24 13:07:04 -0500

    icon outside of logout link.

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -76,7 +76,7 @@ $this->carabiner->display();
 if (is_callable('auth_username')):
 	if (auth_username() !== FALSE):
 ?>
-<p class="navbar-text pull-right"><i class="icon-user icon-white"></i> Logged in as <strong><?=auth_username()?></strong>. <a href="<?=base_url()?>home/logout"><i class="icon-share icon-white"></i> Logout</a></p>
+<p class="navbar-text pull-right"><i class="icon-user icon-white"></i> Logged in as <strong><?=auth_username()?></strong>. <i class="icon-share icon-white"></i> <a href="<?=base_url()?>home/logout">Logout</a></p>
 <?php 
 	endif; 
 endif;
