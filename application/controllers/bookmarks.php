28e903f101095162bf2b2a769cbbbf463964c9bd
Mike Funk <mfunk@xulonpress.com>
2012-02-26 22:15:56 -0500

    reverted to no hmvc because of testing failing.

--- application/controllers/bookmarks.php
+++ application/controllers/bookmarks.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
- * bookmarks
+ * bookmarks 
  * 
  * All methods for bookmarks. All methods are restricted via __construct().
  * 
