735da9497f0617f4396b17b3a06a8322fcbed6ea
Mike Funk <mfunk@xulonpress.com>
2012-03-05 12:29:56 -0500

    more email debug.

--- application/controllers/mail_test.php
+++ application/controllers/mail_test.php
@@ -35,12 +35,18 @@ class mail_test extends CI_Controller
 	 */
 	public function index()
 	{
-		// subject, msg, send
 		$this->load->library('email');
-		$this->email->from('admin@bookymark.com');
-		$this->email->to('mikedfunk@gmail.com');
-		$this->email->subject('subject');
-		$this->email->message('message');
+		
+		$this->email->initialize(array('mailtype' => 'text'));
+
+		$this->email->from('your@example.com', 'Your Name');
+		$this->email->to('mikedfunk@gmail.com'); 
+// 		$this->email->cc('another@another-example.com'); 
+// 		$this->email->bcc('them@their-example.com'); 
+		
+		$this->email->subject('Email Test');
+		$this->email->message('Testing the email class.');	
+		
 		$this->email->send();
 		
 		echo $this->email->print_debugger();
