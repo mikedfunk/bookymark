8a84e442e567e79cae9261865d42f1fb326a595f
Mike Funk <mfunk@xulonpress.com>
2012-03-05 11:15:45 -0500

    added mail test.

---
+++ application/controllers/mail_test.php
@@ -0,0 +1,53 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * mail_test
+ * 
+ * Description
+ * 
+ * @license		Copyright Xulon Press, Inc. All Rights Reserved.
+ * @author		Xulon Press
+ * @link		http://xulonpress.com
+ * @email		info@xulonpress.com
+ * 
+ * @file		mail_test.php
+ * @version		1.0
+ * @date		03/05/2012
+ * 
+ * Copyright (c) 2012
+ */
+
+// --------------------------------------------------------------------------
+
+/**
+ * mail_test class.
+ * 
+ * @extends CI_Controller
+ */
+class mail_test extends CI_Controller
+{
+	// --------------------------------------------------------------------------
+	
+	/**
+	 * index function.
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function index()
+	{
+		// subject, msg, send
+		$this->load->library('email');
+		$this->email->from('admin@bookymark.com');
+		$this->email->to('mikedfunk@gmail.com');
+		$this->email->subject('subject');
+		$this->email->message('message');
+		$this->email->send();
+		
+		echo $this->email->print_debugger();
+	}
+	
+	// --------------------------------------------------------------------------
+}
+
+/* End of file mail_test.php */
+/* Location: ./bookymark/application/controllers/mail_test.php */
