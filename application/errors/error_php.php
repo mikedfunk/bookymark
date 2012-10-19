bc697296fc17732118c4ca4e122248383b6bb1e6
Mike Funk <mfunk@christianpublishing.com>
2012-10-19 11:09:07 -0400

    updated error php template

--- application/errors/error_php.php
+++ application/errors/error_php.php
@@ -11,69 +11,15 @@
 
  // --------------------------------------------------------------------------
 ?>
-<!DOCTYPE html>
-<html lang="en" class="">
-  <head>
-    <meta charset="utf-8">
-    <title>Error</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
-<!--<link type="text/css" href="assets/css/styles.less" />-->
-<link type="text/css" rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css" />
-<style type="text/css">
-section {
-	margin-top: 60px;
-}
-</style>
-
-<script type="text/javascript" src="/assets/js/scripts.js"></script>
-<script type="text/javascript" src="/assets/bootstrap/js/bootstrap.js"></script>
-
-    <!-- HTML5 shim, for IE6-8 support of HTML elements -->
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-
-	<!--[if IE]><![endif]-->
-
-    <!-- fav and touch icons -->
-    <link rel="shortcut icon" href="/assets/img/favicon.ico">
-  </head>
-  <body>
-
-<div class="navbar navbar-fixed-top">
-<div class="navbar-inner">
-    <div class="fill">
-      <div class="container">
-        <a class="brand" href="">Bookymark</a>
-        </div><!--container-->
-        </div><!--fill-->
-        </div><!--navbar-inner-->
-</div><!--navbar-->
-
-<section>
-<div class="container">
-  <h1>An Error Was Encountered</h1>
-<!--Severity: <?php echo $severity; ?>-->
-<p>Message:  <?php echo $message; ?></p>
-<!--Filename: <?php echo $filepath; ?>-->
-<!--Line Number: <?php echo $line; ?>-->
-</div><!--container-->
-</section>
-  <footer>
-<div class="footer">
-<hr />
-<div class="container">
-<p>By <a href="http://mikefunk.com">Mike Funk</a> <a class="pull-right" href="http://www.apache.org/licenses/LICENSE-2.0">Apache License 2.0</p>
-</div><!--container-->
-</div><!--footer-->
-</footer>
-<a href="https://github.com/mikedfunk/bookymark"><img style="position: fixed; z-index: 9999; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub"></a>
-  </body>
-</html>
+<div class="alert alert-block alert-error">
+  <button type="button" class="close" data-dismiss="alert">×</button>
+  <h4>An Error Was Encountered</h4>
+  <hr>
+  <p><strong>Message:</strong> <?=$message?></p>
+  <p><strong>Line Number:</strong> <?=$line?></p>
+  <p><strong>File Path:</strong> <?=$filepath?></p>
+  <p><strong>Severity:</strong> <?=$severity?></p>
+</div>
 <?php
 /* End of file error_php.php */
 /* Location: ./application/errors/error_php.php */
