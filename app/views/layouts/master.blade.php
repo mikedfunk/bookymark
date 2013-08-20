2fcff47cd94e519ee8097c7cc033ef9717d786a1
Mike Funk <mfunk@christianpublishing.com>
2013-08-20 12:42:11 -0400

    added bootstrap suggested markup to master layout

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
-<html>
+<html lang="en">
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 @stylesheets('application')
 @yield('html_head_content')
 </head>
