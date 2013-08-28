a987f41b4de45c8a887f2f2feeaeec5f51fd1454
Mike Funk <mfunk@christianpublishing.com>
2013-08-28 16:58:02 -0400

    updated home view to show additional technologies

--- app/views/home/home_index.blade.php
+++ app/views/home/home_index.blade.php
@@ -24,28 +24,46 @@
   </div><!--col-->
 </div><!--row-->
 <div class="row">
-  <div class="col-lg-3">
+  <div class="col-lg-4">
     <h2>Laravel</h2>
     <p>You can use this as a boilerplate <a href="http://laravel.com">Laravel 4</a>
     project. It has authentication, pagination, and CRUD.</p>
   </div><!--col-->
-  <div class="col-lg-3">
+  <div class="col-lg-4">
     <h2>PSR-2</h2>
     <p>All classes are in app/src and are autoloaded by namespace. All other language
     features comform to <a href="https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-2-coding-style-guide.md">PSR-2</a>
     standards. Code portability and defined standards FTW!</p>
   </div><!--col-->
-  <div class="col-lg-3">
+  <div class="col-lg-4">
     <h2>Unit Tests</h2>
     <p>This was written with tests first. You might find some useful
     <a href="http://phpunit.de">PHPUnit</a> examples
-    in app/tests.</p>
+    in app/tests. Mocks powered by <a href="https://github.com/padraic/mockery">Mockery</a>.</p>
   </div><!--col-->
-  <div class="col-lg-3">
+</div><!--row-->
+<div class="row">
+  <div class="col-lg-4">
+    <h2>Continuous Integration</h2>
+    <p>
+    On push to master, it checks unit tests, verifies PSR-2 code standards, and
+    deploys <em>if</em> they are successful with <a href="http://travis-ci.org">Travis-CI</a>.
+    </p>
+  </div><!--col-->
+  <div class="col-lg-4">
     <h2>Twitter Bootstrap</h2>
     <p>
     This uses all <a href="http://getbootstrap.org">Twitter Bootstrap 3</a>
     responsive markup. Feel free to reuse snippets.
+    </p>
+  </div><!--col-->
+  <div class="col-lg-4">
+    <h2>Google PageSpeed</h2>
+    <p>
+    All js/css files are combined, minified, and cached via
+    <a href="https://github.com/jasonlewis/basset">Basset</a>. All assets are gzipped
+    and have far-future expiration dates. These are some of the factors for fast
+    front-end performance as measured by <a href="https://developers.google.com/speed/pagespeed/">Google PageSpeed</a>.
     </p>
   </div><!--col-->
 </div><!--row-->
