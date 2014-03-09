1585e48b89ba1f787423d7dbf7a82a1ae55a6554
Mike Funk <mike@mikefunk.com>
2014-03-09 03:13:58 -0700

    working

---
+++ app/cache/test/twig/76/49/00bf161a01074bccd87e092e1e3be6a999be0e5ea8bc371ff71b7547d7dd.php
@@ -0,0 +1,57 @@
+<?php
+
+/* AcmeDemoBundle:Secured:layout.html.twig */
+class __TwigTemplate_764900bf161a01074bccd87e092e1e3be6a999be0e5ea8bc371ff71b7547d7dd extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("AcmeDemoBundle::layout.html.twig");
+
+        $this->blocks = array(
+            'content_header_more' => array($this, 'block_content_header_more'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "AcmeDemoBundle::layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_content_header_more($context, array $blocks = array())
+    {
+        // line 4
+        echo "    ";
+        $this->displayParentBlock("content_header_more", $context, $blocks);
+        echo "
+    <li>logged in as <strong>";
+        // line 5
+        echo twig_escape_filter($this->env, (($this->getAttribute((isset($context["app"]) ? $context["app"] : $this->getContext($context, "app")), "user")) ? ($this->getAttribute($this->getAttribute((isset($context["app"]) ? $context["app"] : $this->getContext($context, "app")), "user"), "username")) : ("Anonymous")), "html", null, true);
+        echo "</strong> - <a href=\"";
+        echo $this->env->getExtension('routing')->getPath("_demo_logout");
+        echo "\">Logout</a></li>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "AcmeDemoBundle:Secured:layout.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  36 => 5,  28 => 3,  46 => 8,  40 => 6,  37 => 5,  31 => 4,  26 => 11,);
+    }
+}
---
+++ app/cache/test/twig/f9/d9/7aaa6f9da581ce8e5c822f259e2a59775431dea6f004f8e467d70609a72c.php
@@ -0,0 +1,60 @@
+<?php
+
+/* AcmeDemoBundle:Demo:hello.html.twig */
+class __TwigTemplate_f9d97aaa6f9da581ce8e5c822f259e2a59775431dea6f004f8e467d70609a72c extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("AcmeDemoBundle::layout.html.twig");
+
+        $this->blocks = array(
+            'title' => array($this, 'block_title'),
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "AcmeDemoBundle::layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 9
+        $context["code"] = $this->env->getExtension('demo')->getCode($this);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_title($context, array $blocks = array())
+    {
+        echo twig_escape_filter($this->env, ("Hello " . (isset($context["name"]) ? $context["name"] : $this->getContext($context, "name"))), "html", null, true);
+    }
+
+    // line 5
+    public function block_content($context, array $blocks = array())
+    {
+        // line 6
+        echo "    <h1>Hello ";
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : $this->getContext($context, "name")), "html", null, true);
+        echo "!</h1>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "AcmeDemoBundle:Demo:hello.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  40 => 6,  37 => 5,  31 => 3,  26 => 9,);
+    }
+}
