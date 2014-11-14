7c1b83b694cf33b2b955025904d6ffbbb51ee116
Mike Funk <mike@mikefunk.com>
2014-11-14 12:13:24 -0800

    starting from scratch

--- src/MikeFunk/Bookymark/BookmarksBundle/MikeFunkBookymarkBookmarksBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MikeFunk\Bookymark\BookmarksBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MikeFunkBookymarkBookmarksBundle extends Bundle
-{
-}
--- src/MikeFunk/Bookymark/SiteBundle/MikeFunkBookymarkSiteBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MikeFunk\Bookymark\SiteBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MikeFunkBookymarkSiteBundle extends Bundle
-{
-}
