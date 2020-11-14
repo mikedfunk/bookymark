d2d5a93d702a58e654529db9ffbd057c9b8bae9e
Mike Funk <mike@mikefunk.com>
2020-11-13 19:23:27 -0800

    working

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
@@ -8,11 +10,9 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // \Bookymark\Models\User::factory(10)->create();
     }
 }
